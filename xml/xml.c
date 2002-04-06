4d17ac8027011c6f008c43d7c3466eec760c3f35
Tim Long <timl@sourceforge.net>
2002-04-06 01:42:48 +0000

    Upgrade to ici4 and OO interface. Only Win32 build is done yet.

--- xml/xml.c
+++ xml/xml.c
@@ -1,5 +1,5 @@
 /*
- * $Id: xml.c,v 1.1.1.1 1999/09/08 01:27:26 andy Exp $
+ * $Id: xml.c,v 1.2 2000/03/11 00:34:40 andy Exp $
  *
  * xml.c - Interface to the Expat XML parser.
  *
@@ -11,225 +11,98 @@
  * Originally by Scott Newham <snaf@zeta.org.au>
  * Complete rewrite by Andy Newman <atrn@zeta.org.au>
  *                        ^ note difference in the names, we're not related!
+ * Another rewrite to make it OO in ICI 4 by Tim Long.
  */
 
-#include "ici.h"
-#include "xmlp.h"
+#include <ici.h>
+#include "icistr.h"
+#include <icistr-setup.h>
+#include <XMLParse.h>
+
+objwsup_t    *ici_xml_module;
+objwsup_t    *ici_xml_parser_class;
 
 /*
- * Create a new xml parser with the given Expat parser.
- */
-static xmlp_t *
-new_xmlp(XML_Parser p)
-{
-    xmlp_t	*x;
-
-    if ((x = talloc(xmlp_t)) != NULL)
-    {
-	objof(x)->o_type = &ici_xmlp_type;
-	objof(x)->o_tcode = TC_OTHER;
-	objof(x)->o_flags = 0;
-	objof(x)->o_nrefs = 1;
-	rego(x);
-	x->x_parser   = p;
-	x->x_startf   = NULL;
-	x->x_endf     = NULL;
-	x->x_charf    = NULL;
-	x->x_defaultf = NULL;
-	x->x_pinstf   = NULL;
-	x->x_arg      = objof(&o_null);
-    }
-    return x;
-}
-
-static unsigned long
-mark_xmlp(object_t *o)
-{
-    unsigned long	sz;
-
-    o->o_flags |= O_MARK;
-    sz  = sizeof(xmlp_t) + mark(xmlpof(o)->x_arg);
-    sz += xmlpof(o)->x_startf	== NULL ? 0L : mark(xmlpof(o)->x_startf);
-    sz += xmlpof(o)->x_endf	== NULL ? 0L : mark(xmlpof(o)->x_endf);
-    sz += xmlpof(o)->x_charf	== NULL ? 0L : mark(xmlpof(o)->x_charf);
-    sz += xmlpof(o)->x_defaultf	== NULL ? 0L : mark(xmlpof(o)->x_defaultf);
-    sz += xmlpof(o)->x_pinstf	== NULL ? 0L : mark(xmlpof(o)->x_pinstf);
-    return sz;
-}
-
-/*
- * Free the parser object and loosen all refs to other objects.
- */
-static void
-free_xmlp(object_t *o)
-{
-    XML_ParserFree(xmlpof(o)->x_parser);
-    ici_free(o);
-}
-
-/*
- * AN: Should XML parser object's be atomic? Atomicity being
- * based on the parser structure's address.
+ * Convert the current error in the given XML_Parser object (p) to
+ * an ICI error. Returns 1.
  */
 static int
-cmp_xmlp(object_t *a, object_t *b)
+ici_xml_error(XML_Parser p)
 {
-    /*
-     * Equivalent if the actual Expat parsers are the same.
-     */
-    return xmlpof(a)->x_parser != xmlpof(b)->x_parser;
-}
+    char        *msg;
 
-static unsigned long
-hash_xmlp(object_t *o)
-{
-    return (unsigned long)xmlpof(o)->x_parser * PTR_PRIME_0;
-}
-
-type_t	ici_xmlp_type =
-{
-    mark_xmlp,
-    free_xmlp,
-    hash_xmlp,
-    cmp_xmlp,
-    copy_simple,
-    assign_simple,
-    fetch_simple,
-    "XML_Parser"
-};
-
-/*
- * Error utility
- */
-static int
-xml_error(xmlp_t *x)
-{
-    char	*msg;
-
-    msg = (char *)XML_ErrorString(XML_GetErrorCode(x->x_parser));
-    if (chkbuf(80))
-	error = msg;
+    msg = (char *)XML_ErrorString(XML_GetErrorCode(p));
+    if (ici_chkbuf(80))
+        ici_error = msg;
     else
     {
-	sprintf
-	(
-	    buf,
-	    "line %d, column %d, %s",
-	    XML_GetCurrentLineNumber(x->x_parser),
-	    XML_GetCurrentColumnNumber(x->x_parser),
-	    msg
-	);
-	error = buf;
+        sprintf
+        (
+            ici_buf,
+            "line %d, column %d: %s",
+            XML_GetCurrentLineNumber(p),
+            XML_GetCurrentColumnNumber(p),
+            msg
+        );
+        ici_error = ici_buf;
     }
     return 1;
-}
-
-/**
- * ParserCreate - create a new XML parser object
- */
-FUNC(ParserCreate)
-{
-    XML_Parser	p;
-    xmlp_t	*x;
-
-    if (ici_typecheck(""))
-	return 1;
-    if ((p = XML_ParserCreate(NULL)) == NULL)
-    {
-	error = "XML: failed to create parser";
-	return 1;
-    }
-    if ((x = new_xmlp(p)) == NULL)
-    {
-	XML_ParserFree(p);
-	return 1;
-    }
-    return ici_ret_with_decref(objof(x));
-}
-
-/**
- * SetUserData - set the user data associated with an XML parser object
- */
-FUNC(SetUserData)
-{
-    xmlp_t	*x;
-    object_t	*arg;
-
-    if (ici_typecheck("oo", &x, &arg))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    x->x_arg = arg;
-    return null_ret();
-}
-
-/**
- * GetUserData - get the current user data associated with an XML parser
- */
-FUNC(GetUserData)
-{
-    xmlp_t	*x;
-
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return ici_ret_no_decref(x->x_arg);
 }
 
 /*
  * Generic callback function called by Expat. This in turn calls the specific
  * user callback on the ICI side.
  */
-static void 
+static void
 start_element(void *udata, const char *name, const char **atts)
 {
-    xmlp_t	*x = udata;
-    object_t	*a;
+    ici_handle_t    *h = udata;
+    object_t    *a;
+    object_t    *f;
 
-    if (x->x_startf == NULL || error != NULL)
-	return;
+    if (ici_error != NULL)
+        return;
     /*
      * The attributes are put into a struct object as string objects. The
      * keys are the attribute names, the values the attribute value. If
      * there are no attributes an empty struct is passed.
      */
-    if ((a = objof(new_struct())) == NULL)
-	return;
+    if ((a = objof(ici_struct_new())) == NULL)
+        return;
     if (*atts != NULL)
     {
-	long		i;
-	char		**p;
-	string_t	*n;
-	string_t	*s;
+        long            i;
+        char            **p;
+        string_t        *n;
+        string_t        *s;
 
-	for (p = (char **)atts, i = 0; *p != NULL; p += 2, i += 2)
-	{
-	    assert(p[0] != NULL && p[1] != NULL);
-	    if ((n = new_cname(p[0])) == NULL)
-	    {
-		decref(a);
-		return;
-	    }
-	    if ((s = new_cname(p[1])) == NULL)
-	    {
-		decref(n);
-		decref(a);
-		return;
-	    }
-	    if (assign(a, objof(n), objof(s)))
-	    {
-		decref(s);
-		decref(n);
-		decref(a);
-		return;
-	    }
-	    decref(s);
-	    decref(n);
-	}
+        for (p = (char **)atts, i = 0; *p != NULL; p += 2, i += 2)
+        {
+            assert(p[0] != NULL && p[1] != NULL);
+            if ((n = ici_str_new_nul_term(p[0])) == NULL)
+            {
+                ici_decref(a);
+                return;
+            }
+            if ((s = ici_str_new_nul_term(p[1])) == NULL)
+            {
+                ici_decref(n);
+                ici_decref(a);
+                return;
+            }
+            if (ici_assign(a, objof(n), objof(s)))
+            {
+                ici_decref(s);
+                ici_decref(n);
+                ici_decref(a);
+                return;
+            }
+            ici_decref(s);
+            ici_decref(n);
+        }
     }
-    error = ici_func(objof(x->x_startf), "oso", x->x_arg, name, a);
-    decref(a);
+    ici_method(objof(h), ICIS(start_element), "so", name, a);
+    ici_decref(a);
 }
 
 /*
@@ -239,300 +112,266 @@ start_element(void *udata, const char *name, const char **atts)
 static void
 end_element(void *udata, const char *name)
 {
-    xmlp_t	*x = udata;
+    ici_handle_t    *h = udata;
 
-    if (x->x_endf != NULL && error == NULL)
-	error = ici_func(objof(x->x_endf), "os", x->x_arg, name);
-}
-
-/**
- * SetElementHandler - define callback functions for XML element processing
- */
-FUNC(SetElementHandler)
-{
-    xmlp_t	*x;
-    func_t	*start;
-    func_t	*end;
-
-    switch (NARGS())
-    {
-    case 1:
-	x = xmlpof(ARG(0));
-	start = end = NULL;
-	break;
-
-    default:
-	if (ici_typecheck("ooo", &x, &start, &end))
-	    return 1;
-	if (!funcof(objof(start)))
-	    return ici_argerror(1);
-	if (!funcof(objof(end)))
-	    return ici_argerror(2);
-    }
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    x->x_startf = start;
-    x->x_endf = end;
-    XML_SetElementHandler(x->x_parser, start_element, end_element);
-    XML_SetUserData(x->x_parser, x);
-    return null_ret();
+    if (ici_error != NULL)
+        return;
+    ici_method(objof(h), ICIS(end_element), "s", name);
 }
 
 static void
 character_data(void *udata, const XML_Char *str, int len)
 {
-    xmlp_t	*x = udata;
+    ici_handle_t        *h = udata;
+    string_t        *s;
 
-    if (x->x_charf != NULL && error == NULL)
+    if (ici_error != NULL)
+        return;
+    if ((s = ici_str_new((char *)str, len)) != NULL)
     {
-	string_t	*s;
-
-	if ((s = new_name((char *)str, len)) != NULL)
-	{
-	    error = ici_func(objof(x->x_charf), "oo", x->x_arg, objof(s));
-	    decref(s);
-	}
+        ici_method(objof(h), ICIS(character_data), "o", s);
+        ici_decref(s);
     }
-}
-
-FUNC(SetCharacterDataHandler)
-{
-    xmlp_t	*x;
-    func_t	*fn;
-
-    switch (NARGS())
-    {
-    case 1:
-	x = xmlpof(ARG(0));
-	fn = NULL;
-	break;
-
-    default:
-	if (ici_typecheck("oo", &x, &fn))
-	    return 1;
-	if (!isfunc(objof(fn)))
-	    return ici_argerror(1);
-    }
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    x->x_charf = fn;
-    XML_SetCharacterDataHandler(x->x_parser, character_data);
-    XML_SetUserData(x->x_parser, x);
-    return null_ret();
 }
 
 static void
 default_handler(void *udata, const XML_Char *str, int len)
 {
-    xmlp_t	*x = udata;
+    ici_handle_t            *h = udata;
+    string_t            *s;
 
-    if (x->x_defaultf != NULL && error == NULL)
-    {
-	string_t	*s;
-
-	if ((s = new_name((char *)str, len)) != NULL)
-	{
-	    error = ici_func(objof(x->x_defaultf), "oo", x->x_arg, objof(s));
-	    decref(s);
-	}
-    }
-}
-
-/**
- * SetDefaultHandler - define default callback function
- */
-FUNC(SetDefaultHandler)
-{
-    xmlp_t	*x;
-    func_t	*fn;
-
-    switch (NARGS())
-    {
-    case 1:
-	x = xmlpof(ARG(0));
-	fn = NULL;
-	break;
-
-    default:
-	if (ici_typecheck("oo", &x, &fn))
-	    return 1;
-	if (!isfunc(objof(fn)))
-	    return ici_argerror(1);
-    }
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    x->x_defaultf = fn;
-    XML_SetDefaultHandler(x->x_parser, default_handler);
-    XML_SetUserData(x->x_parser, x);
-    return null_ret();
+    if (ici_error != NULL)
+        return;
+    if ((s = ici_str_new((char *)str, len)) == NULL)
+        return;
+    ici_method(objof(h), ICIS(default_handler), "o", s);
+    ici_decref(s);
 }
 
 static void
 processing_instruction(void *udata, const XML_Char *targ, const XML_Char *data)
 {
-    xmlp_t	*x = udata;
+    ici_handle_t            *h = udata;
+    string_t            *t;
+    string_t            *d;
 
-    if (x->x_pinstf != NULL && error == NULL)
+    if (ici_error != NULL)
+        return;
+    if ((t = ici_str_new_nul_term((char *)targ)) == NULL)
+        return;
+    if ((d = ici_str_new_nul_term((char *)data)) == NULL)
     {
-	string_t	*t;
-	string_t	*d;
-
-	if ((t = new_cname((char *)targ)) == NULL)
-	    return;
-	if ((d = new_cname((char *)data)) == NULL)
-	{
-	    decref(t);
-	    return;
-	}
-	error = ici_func(objof(x->x_pinstf), "ooo", x->x_arg, t, d);
-	decref(t);
-	decref(d);
+        ici_decref(t);
+        return;
     }
+    ici_method(objof(h), ICIS(processing_instruction), "oo", t, d);
+    ici_decref(t);
+    ici_decref(d);
+}
+
+/*
+ * The handle is about to be freed. Free the parser object.
+ */
+static void
+ici_xml_pre_free(ici_handle_t *h)
+{
+    XML_ParserFree((XML_Parser)h->h_ptr);
 }
 
 /**
- * SetProcessingInstructionHandler - set callback for processing instructions
+ * ParserCreate - create a new XML parser object.
+ *
+ * ICI->C parser_class:new -> ici_xml_parser_new
  */
-FUNC(SetProcessingInstructionHandler)
+static int
+ici_xml_parser_new(objwsup_t *klass)
 {
-    xmlp_t	*x;
-    func_t	*fn;
+    XML_Parser          p;
+    ici_handle_t        *h;
 
-    switch (NARGS())
+    if (ici_method_check(objof(klass), 0))
+        return 1;
+    if (ici_typecheck(""))
+        return 1;
+    if ((p = XML_ParserCreate(NULL)) == NULL)
     {
-    case 1:
-	x = xmlpof(ARG(0));
-	fn = NULL;
-	break;
-
-    default:
-	if (ici_typecheck("oo", &x, &fn))
-	    return 1;
-	if (!isfunc(objof(fn)))
-	    return ici_argerror(1);
+        ici_error = "failed to create XML parser";
+        return 1;
     }
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    x->x_pinstf = fn;
-    XML_SetProcessingInstructionHandler(x->x_parser, processing_instruction);
-    XML_SetUserData(x->x_parser, x);
-    return null_ret();
+    XML_SetElementHandler(p, start_element, end_element);
+    XML_SetCharacterDataHandler(p, character_data);
+    XML_SetDefaultHandler(p, default_handler);
+    XML_SetProcessingInstructionHandler(p, processing_instruction);
+    if ((h = ici_handle_new(p, ICIS(XML_Parser), klass)) == NULL)
+        return 1;
+    h->h_pre_free = ici_xml_pre_free;
+    XML_SetUserData(p, h);
+    return ici_ret_with_decref(objof(h));
 }
 
 /**
  * Parse - parse an XML stream
+ *
+ * ICI->C parser_class:Parse -> ici_xml_Parse
  */
-FUNC(Parse)
+static int
+ici_xml_Parse(object_t *inst)
 {
-    xmlp_t	*x;
-    string_t	*s;
-    long	is_final;
+    string_t            *s;
+    long                is_final;
+    XML_Parser          p;
+    ici_handle_t            *h;
 
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), &h, &p))
+        return 1;
     switch (NARGS())
     {
-    case 2:
-	if (ici_typecheck("oo", &x, &s))
-	    return 1;
-	is_final = 1;
-	break;
+    case 1:
+        if (ici_typecheck("o", &s))
+            return 1;
+        is_final = 1;
+        break;
 
     default:
-	if (ici_typecheck("ooi", &x, &s, &is_final))
-	    return 1;
+        if (ici_typecheck("oi", &s, &is_final))
+            return 1;
     }
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    if (!stringof(objof(s)))
-	return ici_argerror(1);
-    error = NULL;
-    if (!XML_Parse(x->x_parser, s->s_chars, s->s_nchars, is_final))
-	return xml_error(x);
-    return error != NULL ? 1 : null_ret();
+    if (!isstring(objof(s)))
+        return ici_argerror(1);
+    ici_error = NULL;
+    if (!XML_Parse(p, s->s_chars, s->s_nchars, is_final))
+        return ici_xml_error(p);
+    return ici_error != NULL ? 1 : null_ret();
 }
 
 /**
  * GetErrorCode - return the current error code from an XML parser object
+ *
+ * ICI->C parser_class:GetErrorCode -> ici_xml_GetErrorCode
  */
-FUNC(GetErrorCode)
+static int
+ici_xml_GetErrorCode(object_t *inst)
 {
-    xmlp_t	*x;
+    XML_Parser           p;
 
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return int_ret(XML_GetErrorCode(x->x_parser));
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    return ici_int_ret(XML_GetErrorCode(p));
 }
 
 /**
  * ErrorString - return the current error string from an XML parser object
+ *
+ * ICI->C parser_class:ErrorString -> ici_xml_ErrorString
  */
-FUNC(ErrorString)
+static int
+ici_xml_ErrorString(object_t *inst)
 {
-    xmlp_t	*x;
+    XML_Parser           p;
 
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return str_ret((char*)XML_ErrorString(XML_GetErrorCode(x->x_parser)));
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    return ici_str_ret((char*)XML_ErrorString(XML_GetErrorCode(p)));
 }
 
 /**
  * GetCurrentLineNumber - return the current line number
+ *
+ * ICI->C parser_class:GetCurrentLineNumber -> ici_xml_GetCurrentLineNumber
  */
-FUNC(GetCurrentLineNumber)
+static int
+ici_xml_GetCurrentLineNumber(object_t *inst)
 {
-    xmlp_t	*x;
+    XML_Parser           p;
 
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return int_ret(XML_GetCurrentLineNumber(x->x_parser));
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    return ici_int_ret(XML_GetCurrentLineNumber(p));
 }
 
 /**
  * GetCurrentColumnNumber - return the current column number
+ *
+ * ICI->C parser_class:GetCurrentColNumber -> ici_xml_GetCurrentColNumber
  */
-FUNC(GetCurrentColumnNumber)
+static int
+ici_xml_GetCurrentColumnNumber(object_t *inst)
 {
-    xmlp_t	*x;
+    XML_Parser           p;
 
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return int_ret(XML_GetCurrentColumnNumber(x->x_parser));
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    return ici_int_ret(XML_GetCurrentColumnNumber(p));
 }
 
 /**
  * GetCurrentByteIndex - return the current byte index
+ *
+ * ICI->C parser_class:GetCurrentByteIndex -> ici_xml_GetCurrentByteIndex
  */
-FUNC(GetCurrentByteIndex)
+static int
+ici_xml_GetCurrentByteIndex(object_t *inst)
 {
-    xmlp_t	*x;
+    XML_Parser           p;
 
-    if (ici_typecheck("o", &x))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    return int_ret(XML_GetCurrentByteIndex(x->x_parser));
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    return ici_int_ret(XML_GetCurrentByteIndex(p));
 }
 
 /**
  * SetBase
+ *
+ * ICI->C parser_class:SetBase -> ici_xml_SetBase
  */
-FUNC(SetBase)
+static int
+ici_xml_SetBase(object_t *inst)
 {
-    xmlp_t	*x;
-    char	*s;
+    char                *s;
+    XML_Parser           p;
 
-    if (ici_typecheck("os", &x, &s))
-	return 1;
-    if (!isxmlp(objof(x)))
-	return ici_argerror(0);
-    if (!XML_SetBase(x->x_parser, (XML_Char *)s))
-	return xml_error(x);
+    if (ici_handle_method_check(inst, ICIS(XML_Parser), NULL, &p))
+        return 1;
+    if (ici_typecheck("s", &s))
+        return 1;
+    if (!XML_SetBase(p, (XML_Char *)s))
+        return ici_xml_error(p);
     return null_ret();
+}
+
+cfunc_t ici_xml_cfuncs[] =
+{
+    {CF_OBJ}
+};
+
+cfunc_t ici_xml_parser_cfuncs[] =
+{
+    {CF_OBJ, "new", ici_xml_parser_new},
+    {CF_OBJ, "SetBase", ici_xml_SetBase},
+    {CF_OBJ, "Parse", ici_xml_Parse},
+    {CF_OBJ, "ErrorString", ici_xml_ErrorString},
+    {CF_OBJ, "GetCurrentByteIndex", ici_xml_GetCurrentByteIndex},
+    {CF_OBJ, "GetCurrentColumnNumber", ici_xml_GetCurrentColumnNumber},
+    {CF_OBJ, "GetCurrentLineNumber", ici_xml_GetCurrentLineNumber},
+    {CF_OBJ, "GetErrorCode", ici_xml_GetErrorCode},
+    {CF_OBJ}
+};
+
+object_t *
+ici_xml_library_init(void)
+{
+    if (init_ici_str())
+        return NULL;
+    if ((ici_xml_module = ici_module_new(ici_xml_cfuncs)) == NULL)
+        return NULL;
+    if ((ici_xml_parser_class = ici_class_new(ici_xml_parser_cfuncs, ici_xml_module)) == NULL)
+        goto fail;
+    if (assign_base(ici_xml_module, ICIS(parser_class), ici_xml_parser_class))
+        goto fail;
+    ici_decref(ici_xml_parser_class);
+    return objof(ici_xml_module);
+
+fail:
+    ici_decref(ici_xml_module);
+    return NULL;
 }
