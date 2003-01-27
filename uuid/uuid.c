69fa72a56e9514d2e7fd80343c148e7d4b4e86ab
Andy Newman <atrn@sourceforge.net>
2003-01-27 11:15:34 +0000

    First checkin of ici 4 version of module

--- uuid/uuid.c
+++ uuid/uuid.c
@@ -1,11 +1,11 @@
 /*
- * $Id: uuid.c,v 1.1 2000/10/13 10:16:45 atrn Exp $
+ * $Id: uuid.c,v 1.2 2000/10/13 10:23:23 atrn Exp $
  *
  * ICI UUID library interface. Placed in the PUBLIC DOMAIN, A.Newman, October 2000.
  *
  */
 
-#include "ici.h"
+#include <ici.h>
 #include <uuid/uuid.h>
 
 typedef struct
@@ -15,12 +15,7 @@ typedef struct
 }
 ici_uuid_t;
 
-inline static
-ici_uuid_t *
-uuidof(void *p)
-{
-    return (ici_uuid_t *)p;
-}
+static int uuid_tcode = 0;
 
 static unsigned long
 mark_uuid(object_t *o)
@@ -29,23 +24,29 @@ mark_uuid(object_t *o)
     return sizeof (ici_uuid_t);
 }
 
+static void
+free_uuid(object_t *o)
+{
+    ici_tfree(o, ici_uuid_t);
+}
+
 static type_t
 ici_uuid_type =
 {
     mark_uuid,
-    free_simple,
-    hash_unique,
-    cmp_unique,
-    copy_simple,
-    assign_simple,
-    fetch_simple,
-    "UUID"
+    free_uuid,
+    ici_hash_unique,
+    ici_cmp_unique,
+    ici_copy_simple,
+    ici_assign_fail,
+    ici_fetch_fail,
+    "uuid"
 };
 
 inline static int
 isuuid(object_t *o)
 {
-    return o->o_type == &ici_uuid_type;
+    return ici_typeof(o) == &ici_uuid_type;
 }
 
 static ici_uuid_t *
@@ -53,19 +54,17 @@ new_uuid(uuid_t uuid)
 {
     ici_uuid_t	*u;
 
-    if ((u = talloc(ici_uuid_t)) != NULL)
+    if ((u = ici_talloc(ici_uuid_t)) != NULL)
     {
-	objof(u)->o_tcode = TC_OTHER;
-	objof(u)->o_nrefs = 1;
-	objof(u)->o_flags = 0;
-	objof(u)->o_type  = &ici_uuid_type;
-	rego(u);
-	memcpy(u->u_uid, uuid, 16);
+        ICI_OBJ_SET_TFNZ(u, uuid_tcode, 0, 1, sizeof *u);
+	ici_rego(u);
+	memcpy(u->u_uid, uuid, sizeof uuid);
     }
     return u;
 }
 
-FUNC(clear)
+static int
+ici_uuid_clear(void)
 {
     ici_uuid_t	*u;
 
@@ -74,10 +73,11 @@ FUNC(clear)
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
     uuid_clear(u->u_uid);
-    return null_ret();
+    return ici_null_ret();
 }
 
-FUNC(compare)
+static int
+ici_uuid_compare(void)
 {
     ici_uuid_t	*u;
     ici_uuid_t	*v;
@@ -88,10 +88,11 @@ FUNC(compare)
 	return ici_argerror(0);
     if (!isuuid(objof(v)))
 	return ici_argerror(1);
-    return int_ret(uuid_compare(u->u_uid, v->u_uid));
+    return ici_int_ret(uuid_compare(u->u_uid, v->u_uid));
 }
 
-FUNC(copy)
+static int
+ici_uuid_copy(void)
 {
     ici_uuid_t	*u;
     uuid_t	uu2;
@@ -104,7 +105,8 @@ FUNC(copy)
     return ici_ret_with_decref(objof(new_uuid(uu2)));
 }
 
-FUNC(generate)
+static int
+ici_uuid_generate(void)
 {
     uuid_t	uu;
 
@@ -112,7 +114,8 @@ FUNC(generate)
     return ici_ret_with_decref(objof(new_uuid(uu)));
 }
 
-FUNC(generate_random)
+static int
+ici_uuid_generate_random(void)
 {
     uuid_t	uu;
 
@@ -120,7 +123,8 @@ FUNC(generate_random)
     return ici_ret_with_decref(objof(new_uuid(uu)));
 }
 
-FUNC(generate_time)
+static int
+ici_uuid_generate_time(void)
 {
     uuid_t	uu;
 
@@ -128,7 +132,8 @@ FUNC(generate_time)
     return ici_ret_with_decref(objof(new_uuid(uu)));
 }
 
-FUNC(is_null)
+static int
+ici_uuid_is_null(void)
 {
     ici_uuid_t	*u;
 
@@ -136,10 +141,11 @@ FUNC(is_null)
 	return 1;
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
-    return int_ret(uuid_is_null(u->u_uid));
+    return ici_int_ret(uuid_is_null(u->u_uid));
 }
 
-FUNC(parse)
+static int
+ici_uuid_parse(void)
 {
     char	*str;
     uuid_t	uu;
@@ -148,13 +154,14 @@ FUNC(parse)
 	return 1;
     if (uuid_parse(str, uu) == -1)
     {
-	error = "invalid UUID";
+	ici_error = "invalid uuid";
 	return 1;
     }
     return ici_ret_with_decref(objof(new_uuid(uu)));
 }
 
-FUNC(unparse)
+static int
+ici_uuid_unparse(void)
 {
     ici_uuid_t	*u;
     char	mybuf[64];
@@ -164,10 +171,11 @@ FUNC(unparse)
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
     uuid_unparse(u->u_uid, mybuf);
-    return str_ret(mybuf);
+    return ici_str_ret(mybuf);
 }
 
-FUNC(time)
+static int
+ici_uuid_time(void)
 {
     ici_uuid_t		*u;
     time_t		t;
@@ -176,10 +184,11 @@ FUNC(time)
 	return 1;
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
-    return int_ret(uuid_time(u->u_uid, NULL));
+    return ici_int_ret(uuid_time(u->u_uid, NULL));
 }
 
-FUNC(type)
+static int
+ici_uuid_typef(void)
 {
     ici_uuid_t	*u;
 
@@ -187,10 +196,11 @@ FUNC(type)
 	return 1;
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
-    return int_ret(uuid_type(u->u_uid));
+    return ici_int_ret(uuid_type(u->u_uid));
 }
 
-FUNC(variant)
+static int
+ici_uuid_variant(void)
 {
     ici_uuid_t	*u;
 
@@ -198,5 +208,32 @@ FUNC(variant)
 	return 1;
     if (!isuuid(objof(u)))
 	return ici_argerror(0);
-    return int_ret(uuid_variant(u->u_uid));
+    return ici_int_ret(uuid_variant(u->u_uid));
+}
+
+static cfunc_t ici_uuid_cfuncs[] =
+{
+    {CF_OBJ, "clear", ici_uuid_clear},
+    {CF_OBJ, "compare", ici_uuid_compare},
+    {CF_OBJ, "copy", ici_uuid_copy},
+    {CF_OBJ, "generate", ici_uuid_generate},
+    {CF_OBJ, "generate_random", ici_uuid_generate_random},
+    {CF_OBJ, "generate_time", ici_uuid_generate_time},
+    {CF_OBJ, "is_null", ici_uuid_is_null},
+    {CF_OBJ, "parse", ici_uuid_parse},
+    {CF_OBJ, "unparse", ici_uuid_unparse},
+    {CF_OBJ, "time", ici_uuid_time},
+    {CF_OBJ, "type", ici_uuid_typef},
+    {CF_OBJ, "variant", ici_uuid_variant},
+    {CF_OBJ}
+};
+
+object_t *
+ici_uuid_library_init(void)
+{
+    if (ici_interface_check(ICI_VER, ICI_BACK_COMPAT_VER, "uuid"))
+        return NULL;
+    if ((uuid_tcode = ici_register_type(&ici_uuid_type)) == 0)
+        return NULL;
+    return objof(ici_module_new(ici_uuid_cfuncs));
 }
